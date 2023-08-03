0f9fa854436344c78ba4a818be25420d73c51048
ntsinh <ntsinh@DESKTOP-G4BLDFT>
2023-08-03 15:22:00 +0700

    Add project files.

---
+++ UserControl2/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace UserControl2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.customButton1 = new UserControl2.CustomButton();
+            this.userControl12 = new UserControl2.UserControl1();
+            this.userControl11 = new UserControl2.UserControl1();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(335, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel1.Location = new System.Drawing.Point(-2, -11);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(294, 389);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(35, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(86, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(375, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LOGIN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(326, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "UserName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(335, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(326, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // customButton1
+            // 
+            this.customButton1.BackColor = System.Drawing.Color.Orange;
+            this.customButton1.BackgroundColor = System.Drawing.Color.Orange;
+            this.customButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.customButton1.BorderRadius = 5;
+            this.customButton1.BorderSize = 0;
+            this.customButton1.FlatAppearance.BorderSize = 0;
+            this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customButton1.ForeColor = System.Drawing.Color.White;
+            this.customButton1.Location = new System.Drawing.Point(363, 260);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(150, 40);
+            this.customButton1.TabIndex = 6;
+            this.customButton1.Text = "Login";
+            this.customButton1.TextColor = System.Drawing.Color.White;
+            this.customButton1.UseVisualStyleBackColor = false;
+            this.customButton1.Click += new System.EventHandler(this.customButton1_Click);
+            // 
+            // userControl12
+            // 
+            this.userControl12.BackColor = System.Drawing.SystemColors.Window;
+            this.userControl12.BorderColor = System.Drawing.Color.Orange;
+            this.userControl12.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.userControl12.BorderRadius = 5;
+            this.userControl12.BorderSize = 2;
+            this.userControl12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl12.ForeColor = System.Drawing.Color.DimGray;
+            this.userControl12.Location = new System.Drawing.Point(326, 200);
+            this.userControl12.Margin = new System.Windows.Forms.Padding(4);
+            this.userControl12.Multiline = false;
+            this.userControl12.Name = "userControl12";
+            this.userControl12.Padding = new System.Windows.Forms.Padding(7);
+            this.userControl12.PasswordChar = true;
+            this.userControl12.Size = new System.Drawing.Size(223, 31);
+            this.userControl12.TabIndex = 5;
+            this.userControl12.Texts = "";
+            this.userControl12.UnderlinedStyle = false;
+            this.userControl12._TextChanged += new System.EventHandler(this.userControl12__TextChanged);
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.SystemColors.Window;
+            this.userControl11.BorderColor = System.Drawing.Color.Orange;
+            this.userControl11.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.userControl11.BorderRadius = 5;
+            this.userControl11.BorderSize = 2;
+            this.userControl11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl11.ForeColor = System.Drawing.Color.DimGray;
+            this.userControl11.Location = new System.Drawing.Point(326, 121);
+            this.userControl11.Margin = new System.Windows.Forms.Padding(4);
+            this.userControl11.Multiline = false;
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Padding = new System.Windows.Forms.Padding(7);
+            this.userControl11.PasswordChar = false;
+            this.userControl11.Size = new System.Drawing.Size(223, 31);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.Texts = "";
+            this.userControl11.UnderlinedStyle = false;
+            this.userControl11._TextChanged += new System.EventHandler(this.userControl11__TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 371);
+            this.Controls.Add(this.customButton1);
+            this.Controls.Add(this.userControl12);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private UserControl1 userControl12;
+        private CustomButton customButton1;
+    }
+}
+
---
+++ UserControl2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControl2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void userControl11__TextChanged(object sender, EventArgs e)
+        {
+            if (userControl11.textBox1_Text.Length >= 5)
+            {
+                label1.Text = "";
+                if (userControl11.textBox1_Text.Contains(" "))
+                {
+                    userControl11.BorderColor = Color.Red;
+                    label1.Text = "Username không được có khoảng trắng";
+                }
+                else
+                {
+                    userControl11.BorderColor = Color.Orange;
+                }
+            }
+            else {
+                userControl11.BorderColor = Color.Red;
+                label1.Text = "UserName Phải từ 5 kí tự";
+            }
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        private void userControl12__TextChanged(object sender, EventArgs e)
+        {
+            if (userControl12.textBox1_Text.Length >= 8 && userControl12.textBox1_Text.Length <= 20)
+            {
+                label4.Text = "";
+                if (userControl12.textBox1_Text.Contains(" "))
+                {
+                    label4.Text = "Password không được chứa khoảng trắng";
+                    userControl12.BorderColor = Color.Red;
+                }
+                else
+                {
+                    label4.Text = "";
+                    userControl12.BorderColor = Color.Orange;
+                }
+            }
+            else
+            {
+                userControl12.BorderColor = Color.Red;
+                label4.Text = "Password phải từ 8 - 20 kí tự";
+            }
+        }
+
+        private void customButton1_Click(object sender, EventArgs e)
+        {
+            if (userControl11.textBox1_Text.Equals("admin") && userControl12.textBox1_Text.Equals("12345678"))
+            {
+              MessageBox.Show("Đăng nhập thành công");
+
+            }
+            else
+            {
+                MessageBox.Show("Sai UserName hoặc Password");
+            }
+        }
+    }
+}
